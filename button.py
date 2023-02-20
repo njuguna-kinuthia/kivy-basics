711fbea1f7eb292dbc7c7caf24346fde3f547675
njuguna-kinuthia <njugunakinuthia013@gmail.com>
2023-02-20 22:09:26 +0300

    sizing and positioning

--- button.py
+++ button.py
@@ -8,7 +8,9 @@ os.environ['KIVY_GL_BACKEND'] = 'angle_sdl2'
 class ButtonApp(App):
 
     def build(self):
-        button = Button(text="click here")
+        button = Button(text="click here",
+                        pos = (50, 50), # 50 pix on the x & y axis
+                        size_hint = (.8, .6)) # sizing relative to screen size
         return button
     
 if __name__ == "__main__":
